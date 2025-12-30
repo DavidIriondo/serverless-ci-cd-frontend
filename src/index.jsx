3e75b7a95d79d0de672c5fd2a0cebd220a752f71
David Iriondo <davidiriondopalacios@gmail.com>
2025-12-30 16:15:41 +0100

    Router added

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import { render } from 'preact';
 import './style.css';
-
+import { Router } from 'preact-router';
 import { ServicesPage } from "./pages/Service/ServicePage.jsx";
 
 export function App() {
-  return <ServicesPage />;
+  return (
+    <Router>
+      <ServicesPage path="/front/dashboard" />
+    </Router>
+  )
 }
+
 
 render(<App />, document.getElementById('app'));
